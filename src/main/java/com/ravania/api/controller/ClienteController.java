60a0f31ebb1f9aa8e1763935bb4b28cd5b76fd85
willianjss <willianjesusdossantos@hotmail.com>
2025-04-28 21:00:26 -0300

    Inclusão de Logs nos Controllers

--- src/main/java/com/ravania/api/controller/ClienteController.java
+++ src/main/java/com/ravania/api/controller/ClienteController.java
@@ -6,19 +6,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
 import java.util.ArrayList;
 import java.util.Objects;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
+    public static final Logger LOGGER = Logger.getLogger(ClienteController.class.getName());
 
     /**
      * <p>
      *     Lista todos os clientes no banco de dados. Se o parâmetro {@code nome} for fornecido, a lista retornada é
      *     filtrada por nome.
      * </p>
-     * @param nome
+     * @param nome do cliente, ou parte dele.
      * @return É retornado uma resposta {@code ResponseEntity} com uma lista de clientes do tipo {@code ArrayList<ClienteDto>}.
      */
     @GetMapping
@@ -26,15 +29,20 @@ public class ClienteController {
         if(Objects.isNull(nome)) {
             try {
                 ArrayList<ClienteDto> listaClientes = new ClienteDao().obterListaClientes();
+                LOGGER.log(Level.FINE,"Lista de clientes retornada com sucesso.");
                 return ResponseEntity.ok(listaClientes);
             } catch (Exception e) {
+                assert LOGGER != null;
+                LOGGER.log(Level.WARNING, "Ocorreu um erro ao buscar os clientes no banco de dados: "+e.getMessage());
                 return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
             }
         } else {
             try {
                 ArrayList<ClienteDto> listaClientes = new ClienteDao().obterListaClientes(nome);
+                LOGGER.log(Level.FINE,"Lista de clientes retornada com sucesso.");
                 return ResponseEntity.ok(listaClientes);
             } catch (Exception e) {
+                LOGGER.log(Level.WARNING, "Ocorreu um erro ao buscar os clientes no banco de dados: "+e.getMessage());
                 return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
             }
         }
@@ -44,15 +52,17 @@ public class ClienteController {
      * <p>
      *     Este método inclui um cliente no banco de dados.
      * </p>
-     * @param clienteDto
+     * @param clienteDto preenchido com nome, data de nascimento e cpf.
      * @return O método retorna um {@code ResponseEntity}, que pode ser {@code HttpStatus.CREATED} (201) ou {@code HttpStatus.UNPROCESSABLE_ENTITY}.
      */
     @PostMapping("/incluir")
     private ResponseEntity<ClienteDto> incluirClientes(@RequestBody final ClienteDto clienteDto){
         ClienteDao clienteDao = new ClienteDao();
         if(Objects.equals(clienteDao.incluirCliente(clienteDto), true)){
+            LOGGER.log(Level.FINE, "Cliente incluído com sucesso no banco de dados.");
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } else {
+            LOGGER.log(Level.WARNING, "Ocorreu um erro ao criar o cliente.");
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
         }
     }
--- src/main/java/com/ravania/api/controller/TransacaoController.java
+++ src/main/java/com/ravania/api/controller/TransacaoController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -13,12 +15,13 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/transacoes")
 public class TransacaoController {
+    public static final Logger LOGGER = Logger.getLogger(TransacaoController.class.getName());
 
     /**
      * <p>
      *     Lista todas as transações no banco de dados, caso o parâmetro data seja fornecido, as transações são filtradas por data.
      * </p>
-     * @param data
+     * @param data no formato AAAA/MM/DD.
      * @return Retorna uma lista {@code ResponseEntity} com uma lista de transações {@code ArrayList<TransacaoDto>}.
      */
     @GetMapping
@@ -26,17 +29,22 @@ public class TransacaoController {
         if (Objects.isNull(data)) {
             try {
                 ArrayList<TransacaoDto> listaTransacoes = new TransacaoDao().obterListaTransacoes();
+                LOGGER.log(Level.FINE,"Lista de transações retornada com sucesso.");
                 return ResponseEntity.ok(listaTransacoes);
             } catch (Exception e) {
                 System.out.println(e.getMessage());
+                assert LOGGER != null;
+                LOGGER.log(Level.WARNING, "Ocorreu um erro ao buscar as transações no banco de dados: "+e.getMessage());
                 return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
             }
         } else {
             try {
                 ArrayList<TransacaoDto> listaTransacoes = new TransacaoDao().obterListaTransacoes(data);
+                LOGGER.log(Level.FINE,"Lista de transações retornada com sucesso.");
                 return ResponseEntity.ok(listaTransacoes);
             } catch (Exception e) {
                 System.out.println(e.getMessage());
+                LOGGER.log(Level.WARNING, "Ocorreu um erro ao buscar as transações no banco de dados: "+e.getMessage());
                 return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
             }
         }
@@ -45,17 +53,24 @@ public class TransacaoController {
     /**
      * <p>
      *     Inlcui uma transacão no banco de dados.
+     *     O sistema não aceitará datas fornecidas no corpo da mensagem, ele substituirá a data e hora pela atual.
      * </p>
-     * @param transacaoDto
+     * @param transacaoDto preenchido com status, valor, cvv e número do cartão.
      * @return Retorna uma resposta do tipo {@code HttpStatus.CREATED}(201) ou {@code HttpStatus.UNPROCESSABLE_ENTITY}
      */
     @PostMapping("/incluir")
     public ResponseEntity<TransacaoDto> incluirTransacao(@RequestBody final TransacaoDto transacaoDto){
         TransacaoDao transacaoDao = new TransacaoDao();
         transacaoDto.setDataTransacao(LocalDateTime.now());
+        if(transacaoDto.getValor() <= -1){
+            LOGGER.log(Level.WARNING,"O valor da transação deve ser positivo ou igual a zero.");
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        }
         if(Objects.equals(transacaoDao.criarTransacao(transacaoDto), true)) {
+            LOGGER.log(Level.FINE, "Transação incluída com sucesso no banco de dados.");
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } else {
+            LOGGER.log(Level.WARNING, "Ocorreu um erro ao criar a transação.");
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
         }
     }
